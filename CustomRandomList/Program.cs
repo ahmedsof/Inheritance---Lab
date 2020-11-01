09726b03a86b2294f9e8a1f7d5929d23dfb269d4
Ahmed Softev <ahmed_v@abv.bg>
2020-11-01 23:07:54 +0200

    C#

---
+++ CustomRandomList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("metko");
+            list.Add("rifko");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
